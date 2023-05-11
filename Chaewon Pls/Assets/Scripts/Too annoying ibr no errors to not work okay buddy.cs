e3adeda006e054f0232554cd3e3b509d24c789fb
Shou <yyousaf2021@elam.co.uk>
2023-05-11 21:45:05 +0100

    new

---
+++ Chaewon Pls/Assets/Scripts/Too annoying ibr no errors to not work okay buddy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tooannoyingibrnoerrorstonotworkokaybuddy : MonoBehaviour
+{
+    //Ibr this stuff too long u do u if u wanna try got to 26 min and was bless but icl it
+    //defo stopped working around 16 or something
+}
